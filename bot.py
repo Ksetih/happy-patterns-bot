4a9a51c002ec6d36a454190c0c8e12a15bb1a667
Ksetih <117385488+Ksetih@users.noreply.github.com>
2026-06-11 21:10:33 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Hi! I'm JoyMap.\n\n"
+        "Soon I'll help you discover what makes you happier."
+    )
+
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
